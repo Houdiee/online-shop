92f8c379746c5586cf5fe77ebb539efad07ed8c4
Kerim <kerimugurlu24@gmail.com>
2025-07-19 18:08:42 +0900

    perfect spacing

--- Frontend/src/components/ProductCard.tsx
+++ Frontend/src/components/ProductCard.tsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
 import { Card, Flex, Image, Typography } from "antd";
 
 export default function ProductCard() {
+  const productName = "straps";
+
   return (
     <>
       <Card variant="borderless" hoverable className="w-70">
         <Flex vertical gap={10}>
-          <Image src="/straps.png" />
+          <Link to={productName}>
+            <Image preview={false} src="/straps.png" />
+          </Link>
           <Flex vertical>
             <Typography.Text className="!text-md">Product Title</Typography.Text>
             <Typography.Text type="secondary">Variant</Typography.Text>
--- Frontend/src/pages/ProductDetails.tsx
+++ Frontend/src/pages/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Carousel, ConfigProvider, Flex, Image } from "antd";
+import { Button, Carousel, ConfigProvider, Flex, Image, Typography } from "antd";
 
 export default function ProductDetails() {
   const images = [
@@ -9,23 +9,53 @@ export default function ProductDetails() {
 
   return (
     <>
-      <Flex justify="center" align="center">
-        <ConfigProvider theme={{ components: { Carousel: { arrowSize: 40 } } }}>
-          <Carousel
-            arrows={true}
-            dots={true}
-            infinite={true}
-            autoplay={true}
-            className="w-100"
-          >
-            {images.map((src, index) => (
-              <div key={index}>
-                <Image preview={false} src={src} />
-              </div>
-            ))}
-          </Carousel >
-        </ConfigProvider>
-      </Flex >
+      <Flex gap={60} className="!mt-30 !ml-50 !mr-50">
+
+        {/* Left side (aka image) */}
+        <Flex flex={1} justify="center" align="center">
+          <ConfigProvider theme={{ components: { Carousel: { arrowSize: 40 } } }}>
+            <Carousel
+              arrows={true}
+              dots={true}
+              infinite={true}
+              autoplay={true}
+              className="w-125"
+            >
+              {images.map((src, index) => (
+                <div key={index}>
+                  <Image preview={false} src={src} />
+                </div>
+              ))}
+            </Carousel >
+          </ConfigProvider>
+        </Flex >
+
+        {/* Right side (aka title, description, variant, etc.) */}
+        <Flex vertical flex={1}>
+
+          <Flex vertical gap={10}>
+            <Typography.Title level={1} className="!m-0">Product Title</Typography.Title>
+            <Typography.Title level={2} type="secondary" className="!m-0">Variant</Typography.Title>
+            <Typography.Title level={2} className="!m-0">$AUD</Typography.Title>
+          </Flex>
+
+          <Typography.Paragraph type="secondary" className="!mt-auto">
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+            Description is not available for this product.
+          </Typography.Paragraph>
+
+          <Button size="large" type="primary" className="!mt-auto">
+            Add to Cart
+          </Button>
+        </Flex>
+
+      </Flex>
     </>
   );
 }
