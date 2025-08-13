00fc3b82738452c493ae628a1bab06a18081a5f0
Kerim <kerimugurlu24@gmail.com>
2025-08-13 19:05:57 +1000

    animation

--- Frontend/src/components/product-details/AddToCartButton.tsx
+++ Frontend/src/components/product-details/AddToCartButton.tsx
@@ -1,28 +1,45 @@
-import { ShoppingCartOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { ShoppingCartOutlined, CheckOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { type AddShoppingCartItemRequest } from "../../types/shopping-cart";
 import axios from "axios";
 import { API_BASE_URL, user } from "../../main";
 
 export default function AddToCartButton({ productVariantId, quantity }: AddShoppingCartItemRequest) {
+  const [buttonState, setButtonState] = useState<"default" | "success">('default');
+
   const handleAddToCart = async () => {
-    await axios.post(`${API_BASE_URL}/users/${user.id}/shoppingcart`, {
-      ProductVariantId: productVariantId,
-      Quantity: quantity,
-    });
-    console.log(user);
+    try {
+      await axios.post(`${API_BASE_URL}/users/${user.id}/shoppingcart`, {
+        ProductVariantId: productVariantId,
+        Quantity: quantity,
+      });
+
+      console.log(user);
+
+      setButtonState('success');
+
+      setTimeout(() => {
+        setButtonState('default');
+      }, 3000);
+
+    } catch (error) {
+      console.error("Failed to add item to cart:", error);
+    }
   };
 
   return (
     <>
       <Button
         size="large"
-        type="primary"
-        className="!mt-auto"
+        type={buttonState === 'success' ? 'default' : 'primary'}
+        className={`!mt-auto ${buttonState === 'success' ? '!bg-green-500 !text-white !border-green-500' : ''}`}
         onClick={handleAddToCart}
       >
-        <ShoppingCartOutlined /> Add to Cart
+        {buttonState === 'success' ? <CheckOutlined /> : <ShoppingCartOutlined />}
+        {buttonState === 'success' ? ' Added to Cart' : ' Add to Cart'}
       </Button>
     </>
   );
 }
+
