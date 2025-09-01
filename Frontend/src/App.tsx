2aca4836801a569329c4dc6bcacdb87b084d6e67
Kerim <kerimugurlu24@gmail.com>
2025-09-01 15:29:38 +1000

    protected route

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,29 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import AccountDetails from './pages/AccountDetails.tsx';
 import Dashboard from './pages/Dashboard.tsx';
 import CreateProduct from './pages/CreateProduct.tsx';
 import LoginPage from './pages/Login.tsx';
+import { user } from './main.tsx';
+import { useEffect } from 'react';
+
+const ProtectedRoute = ({ children }: any) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user || user.role !== 'admin') {
+      window.history.back();
+    }
+  }, [navigate]);
+
+  if (user && user.role === 'admin') {
+    return children;
+  }
+
+  return null;
+};
+
 
 function App() {
   return (
@@ -14,9 +33,10 @@ function App() {
       <Route path="/account" element={<AccountDetails />} />
       <Route path="/products" element={<Products />} />
       <Route path="/products/:id/:variantId" element={<ProductDetails />} />
-      <Route path="/admin/dashboard" element={<Dashboard />} />
-      <Route path="/admin/create" element={<CreateProduct />} />
-      <Route path="/admin/edit/:productId" element={<CreateProduct />} />
+
+      <Route path="/admin/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+      <Route path="/admin/create" element={<ProtectedRoute> <CreateProduct /> </ProtectedRoute>} />
+      <Route path="/admin/edit/:productId" element={<ProtectedRoute><CreateProduct /> </ProtectedRoute>} />
     </Routes>
   );
 }
